8d98cdf261cb715cb2c14422a8480f70ef14616c
Rohit Sharma <rohit7.sharma@paytmmall.com>
2018-11-21 19:33:45 +0530

    Now using UTF-8 in the charset

--- EasyVolley/src/main/java/com/easyvolley/NetworkRequest.java
+++ EasyVolley/src/main/java/com/easyvolley/NetworkRequest.java
@@ -5,10 +5,12 @@ import android.text.TextUtils;
 import com.android.volley.Cache;
 import com.android.volley.NetworkResponse;
 import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.easyvolley.interceptors.ResponseInterceptor;
 import com.easyvolley.interceptors.impl.GzipInterceptor;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -137,7 +139,14 @@ public class NetworkRequest extends StringRequest {
             response = i.intercept(response);
         }
 
-        return super.parseNetworkResponse(response);
+        String parsed;
+        try {
+            parsed = new String(response.data, HttpHeaderParser.parseCharset(response.headers, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            parsed = new String(response.data);
+        }
+
+        return Response.success(parsed, HttpHeaderParser.parseCacheHeaders(response));
     }
 
     /**
